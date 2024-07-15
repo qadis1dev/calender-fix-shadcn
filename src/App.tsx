576f14980571d2aa2ea6a40189c011fcf85098d7
qadis1dev <alqadis1devops@gmail.com>
2024-07-16 00:18:09 +0400

    c

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Calendar } from "@/components/ui/calendar";
 const App = () => {
   return (
     <main className="flex h-[100dvh] items-center justify-center">
-      <Calendar captionLayout="dropdown-buttons" fromYear={1990} toYear={2024} />
+      <Calendar captionLayout="dropdown-buttons" fromYear={1900} toYear={2024} />
     </main>
   )
 }
